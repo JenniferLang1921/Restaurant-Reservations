4e5d0b8c47638a83da93bb10a3eb87a73d7776da
Melissa Waltner <melissamwaltner@gmail.com>
2018-04-19 20:46:40 -0500

    Moved html routes into seperate file

--- server.js
+++ server.js
@@ -8,17 +8,7 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/home.html"));
-});
-
-app.get("/reservations", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/reservations.html"));
-});
-
-app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/tables.html"));
-});
+require('./routes/htmlroutes')(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
