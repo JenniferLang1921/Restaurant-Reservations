5ec155ce29010571645db65597edd5e0a4a282a5
p shehan <pshehan@ps-MacBook-Air.local>
2018-04-19 20:56:24 -0500

    adding data

---
+++ data/tables.js
@@ -0,0 +1,27 @@
+var tables = [
+    {
+      name: "Lebron James",
+      phone: "212-800-5555",
+      email: "ljames@gmail.com",
+      uniqueID: 23
+    },
+    {
+        name: "Dwyane Wade",
+        phone: "212-800-5222",
+        email: "dwade@yahoo.com",
+        uniqueID: 3
+    },
+    {
+        name: "Chris Bosh",
+        phone: "212-803-2255",
+        email: "bigboshman@aol.com",
+        uniqueID: 10
+    },
+
+    {
+        name: "Eric Spoelstra",
+        phone: "212-911-3334",
+        email: "ESpo@yahoo.com",
+        uniqueID: 100
+    }
+  ];
